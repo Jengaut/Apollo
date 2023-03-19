b4e575744f14a0ab1d27da5be193240579fb4cde
Jennifer Gauthier <jennifergauthier_1@hotmail.com>
2023-03-19 14:31:30 -0400

    modif

--- assets/php/formulaire.php
+++ assets/php/formulaire.php
@@ -3,25 +3,22 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require 'vendor/autoload.php'; // Charge la librairie PHPMailer
+require 'vendor/autoload.php';
 
-// Vérifie si le formulaire a été soumis
+
 if (isset($_POST['submit'])) {
-    // Vérifie le champ anti-spam honeypot
     if (!empty($_POST['honeypot'])) {
         die('Spam détecté');
     }
 
-    // Récupère les données du formulaire
     $email = $_POST['email'];
     $sujet = $_POST['sujet'];
     $message = $_POST['message'];
+    $submit = $_POST['submit'];
 
-    // Initialise une nouvelle instance de PHPMailer
     $mail = new PHPMailer(true);
 
     try {
-        // Configuration du serveur SMTP
         $mail->isSMTP();
         $mail->Host       = 'smtp.example.com'; // Remplacez par le nom de votre serveur SMTP
         $mail->SMTPAuth   = true;
@@ -30,18 +27,18 @@ if (isset($_POST['submit'])) {
         // $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
         $mail->Port       = 465; // Le port SMTP sécurisé
 
-        // Destinataire et expéditeur du message
+
         $mail->setFrom($email, 'Nom de l\'expéditeur');
         $mail->addAddress('destinataire@example.com', 'Nom du destinataire');
 
-        // Contenu du message
+
         $mail->isHTML(true);
         $mail->Subject = $sujet;
         $mail->Body    = $message;
 
-        // Envoi du message
-        $mail->send();
-        echo 'Le message a été envoyé avec succès';
+
+        $mail->send($submit);
+        header("location:index.html#contact?success=1");
     } catch (Exception $e) {
         echo "Le message n'a pas pu être envoyé. Erreur : {$mail->ErrorInfo}";
     }
